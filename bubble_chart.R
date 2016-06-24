1b1ff3975734f8f0e90ded3b269eb444977a69d5
Anu Rajaram <anurajaram23@gmail.com>
2016-06-24 11:14:19 -0400

    update

--- bubble_chart.R
+++ bubble_chart.R
@@ -24,8 +24,16 @@ radius <- sqrt( crimesfo$popl/ pi )
 symbols(crimesfo$LARCENY.THEFT, crimesfo$ASSAULT, circles=radius, inches=0.35, 
         fg="white", bg="red", xlab="Larceny CrimeRates", 
         ylab="Assault Crimerates")
-
 text(crimesfo$LARCENY.THEFT, crimesfo$ASSAULT, crimesfo$area, cex=0.5)
+
+
+# Similar bubble plot using ggplot library functions.
+ p1 = ggplot(crimesfo, aes(x = area, y = (LARCENY.THEFT/popl)*100), 
+        label = area) + 
+   geom_point(color = "blue", aes(size = LARCENY.THEFT))
+ 
+ p1 + ggtitle("Plot") +
+   xlab("District") + ylab("Theft as % of District Population")
 
 
 
